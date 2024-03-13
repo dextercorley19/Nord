627ca266a99d15cb2ca8b3c3743b54dacb898951
Dexter Corley <dexter.corley@live.com>
2024-03-12 23:28:25 -0700

    Clerk

--- nord-app/app/layout.tsx
+++ nord-app/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs';
 import { Inter } from "next/font/google";
 import "./globals.css";
 
