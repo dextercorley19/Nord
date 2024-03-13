a857a5436192419aba8b8f698fe90e56ce2d0a6a
Dexter Corley <dexter.corley@live.com>
2024-03-12 23:13:06 -0700

    Clerk Key

--- nord-app/app/layout.tsx
+++ nord-app/app/layout.tsx
@@ -16,9 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider 
-    {...pageProps} 
-    publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
